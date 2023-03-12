3c20408ca0ce44228c44b47baf117265f7ad5eef
j1ngzoue <workdrive0306@gmail.com>
2023-03-12 12:39:12 +0900

    feat: add jquery

--- app/frontend/entrypoints/home.tsx
+++ app/frontend/entrypoints/home.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import $ from 'jquery'
+window.$ = $
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div>hoge</div>
